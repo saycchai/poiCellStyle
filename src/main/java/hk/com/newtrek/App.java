16aae301a73fefa6e0bd8734856240e7c899d347
cchai <cnsze@apex-pro.com>
2019-11-26 16:18:19 +0800

    added border, grey color and white color as an indicator that the cell is editable
    Change-Id: Ib57a1e59cce7d4b03c22c5552963af85561ca766

--- src/main/java/hk/com/newtrek/App.java
+++ src/main/java/hk/com/newtrek/App.java
@@ -14,18 +14,20 @@ import java.util.List;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.DataFormat;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import hk.com.newtrek.OrderEntity.ProductEntity;
 import hk.com.newtrek.OrderEntity.ShipmentEntity;
 
 public class App {
-	private static CellStyle unlockedTextCellStyle;
-	private static CellStyle textCellStyle;
+	private static XSSFCellStyle unlockedTextCellStyle;
+	private static XSSFCellStyle textCellStyle;
 	private static final int rowStartIndex = 2;
 	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
 	private static final String password = "password";
@@ -34,14 +36,38 @@ public class App {
 		Path targetPath = Paths.get("./target/output_" + LocalDateTime.now().format(formatter) + ".xlsx");
 		try (OutputStream os = Files.newOutputStream(targetPath);
 				InputStream is = App.class.getClassLoader().getResourceAsStream("template.xlsx");) {
-			Workbook wb = new XSSFWorkbook(is);
+			XSSFWorkbook wb = new XSSFWorkbook(is);
 			DataFormat fmt = wb.createDataFormat();
 			unlockedTextCellStyle = wb.createCellStyle();
 			unlockedTextCellStyle.setLocked(false);
 			unlockedTextCellStyle.setDataFormat(fmt.getFormat("@"));
+			unlockedTextCellStyle.setFillForegroundColor(IndexedColors.WHITE.getIndex());
+			unlockedTextCellStyle.setFillPattern(CellStyle.SOLID_FOREGROUND);
+
+			// border
+			unlockedTextCellStyle.setBorderBottom(XSSFCellStyle.BORDER_THIN);
+			unlockedTextCellStyle.setBottomBorderColor(IndexedColors.BLACK.getIndex());
+			unlockedTextCellStyle.setBorderTop(XSSFCellStyle.BORDER_THIN);
+			unlockedTextCellStyle.setTopBorderColor(IndexedColors.BLACK.getIndex());
+			unlockedTextCellStyle.setBorderRight(XSSFCellStyle.BORDER_THIN);
+			unlockedTextCellStyle.setRightBorderColor(IndexedColors.BLACK.getIndex());
+			unlockedTextCellStyle.setBorderLeft(XSSFCellStyle.BORDER_THIN);
+			unlockedTextCellStyle.setLeftBorderColor(IndexedColors.BLACK.getIndex());
 
 			textCellStyle = wb.createCellStyle();
 			textCellStyle.setDataFormat(fmt.getFormat("@"));
+			textCellStyle.setFillForegroundColor(new XSSFColor(new java.awt.Color(242, 242, 242)));
+			textCellStyle.setFillPattern(CellStyle.SOLID_FOREGROUND);
+
+			// border
+			textCellStyle.setBorderBottom(XSSFCellStyle.BORDER_THIN);
+			textCellStyle.setBottomBorderColor(IndexedColors.BLACK.getIndex());
+			textCellStyle.setBorderTop(XSSFCellStyle.BORDER_THIN);
+			textCellStyle.setTopBorderColor(IndexedColors.BLACK.getIndex());
+			textCellStyle.setBorderRight(XSSFCellStyle.BORDER_THIN);
+			textCellStyle.setRightBorderColor(IndexedColors.BLACK.getIndex());
+			textCellStyle.setBorderLeft(XSSFCellStyle.BORDER_THIN);
+			textCellStyle.setLeftBorderColor(IndexedColors.BLACK.getIndex());
 
 			Sheet sheet = wb.getSheet("Order");
 			int rowIdx = rowStartIndex;
@@ -52,15 +78,16 @@ public class App {
 				final int orderSpanRow = (int) order.getTotalShipmentProduct();
 				int colIdx = 0;
 				int lastOrderColIdx = 5;
+				Row row = null;
+				Cell cell = null;
 
-				Cell cell;
 				for (int i = 0; i <= lastOrderColIdx; i++) {
 					// merged cell region for order fields
 					CellRangeAddress mergedRegion = new CellRangeAddress(rowIdx, rowIdx + orderSpanRow - 1, i, i);
 					sheet.addMergedRegion(mergedRegion);
 				}
 
-				Row row = sheet.createRow(rowIdx);
+				row = getRow(sheet, rowIdx);
 
 				cell = createTextStyleCell(row, colIdx++);
 				cell.setCellValue(order.getOrderNo());
@@ -86,16 +113,13 @@ public class App {
 					final int totalShipCol = 10;
 
 					for (int i = colIdx; i < colIdx + totalShipCol; i++) {
-						// merged cell region for order fields
+						// merged cell region for shipment fields
 						CellRangeAddress mergedRegion = new CellRangeAddress(rowIdx, rowIdx + shipmentSpanRow - 1, i,
 								i);
 						sheet.addMergedRegion(mergedRegion);
 					}
 
-					row = sheet.getRow(rowIdx);
-					if (row == null) {
-						row = sheet.createRow(rowIdx);
-					}
+					row = getRow(sheet, rowIdx);
 
 					cell = createTextStyleCell(row, shipColIdx++);
 					cell.setCellValue(shipment.getShipmentNo());
@@ -108,44 +132,33 @@ public class App {
 
 					cell = createTextStyleCell(row, shipColIdx++);
 					cell.setCellValue(shipment.getExpectedLrd());
-					if ("PROFORMA".equalsIgnoreCase(order.getStatus())) {
-						cell.setCellStyle(unlockedTextCellStyle);
-					}
+					enableCellForProforma(cell, order);
 
 					cell = createTextStyleCell(row, shipColIdx++);
 					cell.setCellValue(shipment.getLrdChangeReason());
 
 					cell = createTextStyleCell(row, shipColIdx++);
 					cell.setCellValue(shipment.getLrdChangeReason());
-					if ("PROFORMA".equalsIgnoreCase(order.getStatus())) {
-						cell.setCellStyle(unlockedTextCellStyle);
-					}
+					enableCellForProforma(cell, order);
 
 					cell = createTextStyleCell(row, shipColIdx++);
 					cell.setCellValue(shipment.getRevisedLrd());
 
 					cell = createTextStyleCell(row, shipColIdx++);
 					cell.setCellValue(shipment.getRevisedLrd());
-					if ("OFFICIAL".equalsIgnoreCase(order.getStatus())) {
-						cell.setCellStyle(unlockedTextCellStyle);
-					}
+					enableCellForOfficialOrder(cell, order);
 
 					cell = createTextStyleCell(row, shipColIdx++);
 					cell.setCellValue(shipment.getReason());
 
 					cell = createTextStyleCell(row, shipColIdx++);
 					cell.setCellValue(shipment.getReason());
-					if ("OFFICIAL".equalsIgnoreCase(order.getStatus())) {
-						cell.setCellStyle(unlockedTextCellStyle);
-					}
+					enableCellForOfficialOrder(cell, order);
 
 					for (ProductEntity product : shipment.getProductEntities()) {
 						int productColIdx = shipColIdx;
 
-						row = sheet.getRow(rowIdx);
-						if (row == null) {
-							row = sheet.createRow(rowIdx);
-						}
+						row = getRow(sheet, rowIdx);
 
 						cell = createTextStyleCell(row, productColIdx++);
 						cell.setCellValue(product.getProductCode());
@@ -164,9 +177,7 @@ public class App {
 
 						cell = createTextStyleCell(row, productColIdx++);
 						cell.setCellValue(product.getRevisedQty());
-						if ("OFFICIAL".equalsIgnoreCase(order.getStatus())) {
-							cell.setCellStyle(unlockedTextCellStyle);
-						}
+						enableCellForOfficialOrder(cell, order);
 
 						rowIdx++;
 					}
@@ -182,14 +193,38 @@ public class App {
 			wb.write(os);
 		}
 
-		System.out.println("... finish generating order excel: " + targetPath.toAbsolutePath().toString() +"...");
+		System.out.println("... finish generating order excel: " + targetPath.toAbsolutePath().toString() + "...");
 	}
-	
-	private static Cell createTextStyleCell(Row row, int col)
-	{
-		Cell cell = row.createCell(col);
-		cell.setCellStyle(textCellStyle);
+
+	private static void enableCellForProforma(final Cell cell, final OrderEntity order) {
+		if ("PROFORMA".equalsIgnoreCase(order.getStatus())) {
+			cell.setCellStyle(unlockedTextCellStyle);
+		}
+	}
+
+	private static void enableCellForOfficialOrder(final Cell cell, final OrderEntity order) {
+		if ("OFFICIAL".equalsIgnoreCase(order.getStatus())) {
+			cell.setCellStyle(unlockedTextCellStyle);
+		}
+	}
+
+	private static Cell createTextStyleCell(final Row row, final int col) {
+		Cell cell = row.getCell(col);
+		if (cell == null) {
+			cell = row.createCell(col);
+			cell.setCellStyle(textCellStyle);
+		}
+
 		return cell;
+	}
+
+	private static Row getRow(final Sheet sheet, final int rowIdx) {
+		Row row = sheet.getRow(rowIdx);
+		if (row == null) {
+			row = sheet.createRow(rowIdx);
+		}
+
+		return row;
 	}
 
 	private static List<OrderEntity> initOrderList() {
